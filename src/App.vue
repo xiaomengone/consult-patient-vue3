3ce927b266dc17b3ccad68cfdb4dddcde51762a5
xiaomeng <764504466@qq.com>
2023-06-07 21:28:02 +0800

    feat: vant移动端适配

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 </script>
 
 <template>
- <div>
-  123
+ <div class="a">
+  <van-button type="primary">主要按钮</van-button>
+<van-button type="success">成功按钮</van-button>
  </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import 'vant/lib/index.css';
+import './styles/main.scss'
+import { Button } from 'vant';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(Button);
 app.mount('#app')
